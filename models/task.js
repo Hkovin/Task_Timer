baf5a2c2b3e6aabf1abffc5b49e4f8b9db896375
Hannah Kovinsky <hkovin@umich.edu>
2021-09-30 02:18:11 -0400

    Update task.js

--- models/task.js
+++ models/task.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    title: String,
-    day: String,
-    time: Number,
-    description: String
+  title: String,
+  day: String,
+  time: Number,
+  description: String,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-module.exports = mongoose.model('Task',  TaskSchema);
+module.exports = mongoose.model("Task", TaskSchema);

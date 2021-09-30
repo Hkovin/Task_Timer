c716d1407c0408e60442f7d664fb9bc5d5957d89
Hannah Kovinsky <hkovin@umich.edu>
2021-09-30 02:07:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,116 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Task = require('./models/task');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const ejsMate = require('ejs-mate');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const Task = require("./models/task");
+const catchAsync = require("./utilities/catchAsync");
+const ejsMate = require("ejs-mate");
+const ExpressError = require("./utilities/ExpressError");
 const app = express();
-const path = require('path');
-const methodOverride = require('method-override');
-app.engine('ejs', ejsMate)//to define layout files
+const session = require("express-session");
+const path = require("path");
+const { taskSchema } = require("./schemas.js");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+const helmet = require("helmet");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+app.engine("ejs", ejsMate); //to define layout files
+const usersRoutes = require("./routes/users");
+const taskRoutes = require("./routes/tasks");
+const mongoSanitize = require("express-mongo-sanitize");
 
-
-mongoose.connect('mongodb://localhost:27017/ScheduleBuilder', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/ScheduleBuilder";
+const secret = "weirdsecret";
+mongoose.connect(dbUrl, {
+  useUnifiedTopology: true,
 });
-
+const MongoStore = require("connect-mongo");
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));//check if error
-db.once("open", () => {//if no error say database connected
-    console.log("Database connected");
+db.on("error", console.error.bind(console, "connection error:")); //check if error
+db.once("open", () => {
+  //if no error say database connected
+  console.log("Database connected");
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({ extended: true }))//pasrse through body
-app.use(methodOverride('_method'));
-app.get('/', async (req, res) => {
-    const tasks =  await Task.find({});
-    res.render('index', {tasks})
- });
+app.use(express.urlencoded({ extended: true })); //pasrse through body
+app.use(methodOverride("_method"));
+app.use(helmet({ contenetSecurityPolicy: false }));
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize());
 
-app.get('/tasks/new', (req,res) => {
-    res.render('tasks/new');
-})
-
-app.post('/tasks', async(req, res) =>{
-    const task = new Task(req.body.task);
-    await task.save();
-    res.redirect(`/tasks/${task._id}`)
-})
-
-app.get('/tasks/:id', async (req, res,) => {
-    const task = await Task.findById(req.params.id)
-    res.render('tasks/show', { task });
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60, //stop unnessessary updates so dont coninuously update page
+  crypto: {
+    secret: "squirrel",
+  },
+});
+store.on("error", function (e) {
+  console.log("Session Store Error", e);
 });
 
+const sessionConfig = {
+  store, //mongo to store info
+  name: "cookieSession", //dont use default name
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    //extra security
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //date.now in milliseconds
+    maxAge: 1000 * 60 * 60 * 24 * 7, //expreation
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser()); //how to serialize a use/ how to store user in session
+passport.deserializeUser(User.deserializeUser());
 
-
-app.get('/tasks/:id/edit', async (req, res,) => {
-    const task = await Task.findById(req.params.id)
-    res.render('tasks/edit', { task });
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-app.put('/tasks/:id', async (req,res) => {
-    const { id } = req.params;
-    const task = await Task.findByIdAndUpdate(id, { ...req.body.task });
-    res.redirect(`/tasks/${task._id}`)
-})
+app.use("/tasks", taskRoutes);
+app.use("/", usersRoutes);
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.delete('/tasks/:id', async (req,res) => {
-    const { id } = req.params;
-    await Task.findByIdAndDelete(id);
-    res.redirect('/');
-})
+app.get("/users/:id", function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    if (err) {
+      console.log(err);
+    }
+    res.render("profile", { user: user });
+  });
+});
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err; //500 is defult value
+  if (!err.message) err.message = "Error! Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
